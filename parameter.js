fc707d3958b4a78bf51ffc5d87639fda6a19a626
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-10 22:12:13 +0100

    paramete of function done

---
+++ parameter.js
@@ -0,0 +1,14 @@
+function square(number){
+    let result;
+    result = number*number;
+    console.log(result);
+}
+square(4);
+
+
+function sumOfTwoNumber(numberOne,numberTwo){
+    let sum;
+    sum = numberOne + numberTwo;
+    console.log(sum);
+}
+sumOfTwoNumber(22,43);
